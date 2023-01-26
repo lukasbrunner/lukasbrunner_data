fadc0d6285e25050ad57f8a118cea29ce6065324
Lukas Brunner <l.brunner@univie.ac.at>
2023-01-26 15:57:18 +0100

    Automatic commit

--- update.sh
+++ update.sh
@@ -3,10 +3,10 @@
 pushd /users/staff/lbrunner/Files/Other/homepage_data/ >/dev/null 2>&1
 
 # newest file
-fn=$(ls -t /users/staff/lbrunner/Coding/tas_to_date/figures/austria/2022/tas_both_austria_2022_*_stepsize-auto_delay-40_size-1000.gif | head -n1) >/dev/null 2>&1
+fn=$(ls -t /users/staff/lbrunner/Coding/tas_to_date/figures/austria/2023/tas_both_austria_2023_*_stepsize-auto_delay-40_size-1000.gif | head -n1) >/dev/null 2>&1
 
 # update if a newer version is available
-/usr/bin/rsync -auv $fn /users/staff/lbrunner/Files/Other/homepage_data/austria/2022/tas_both_austria_2022_newest_stepsize-auto_delay-40_size-1000.gif >/dev/null 2>&1
+/usr/bin/rsync -auv $fn /users/staff/lbrunner/Files/Other/homepage_data/austria/2023/tas_both_austria_2023_newest_stepsize-auto_delay-40_size-1000.gif >/dev/null 2>&1
 
 # try to push potential updates
 git commit -am "Automatic commit" >/dev/null 2>&1
